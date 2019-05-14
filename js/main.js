6c955105e3fc6ffa1f76781b77cc9a4aa3fb2da2
Cristian Torres <cristian.torres19@hotmail.com>
2019-05-13 21:58:01 -0500

    CristianSuggestion

--- js/main.js
+++ js/main.js
@@ -1,33 +1,64 @@
 const cambie = document.querySelector('.toggle');
 const singUp = document.querySelector('.signup');
 const login = document.querySelector('.login');
+const dispare = document.querySelector('.dispare');
 let i = 1;
+let tiempoInicial = 50;
+let differenceTime = 100;
+let temp;
+dispare.addEventListener('click', () => {
+
+    metaTiempo('.lia', 'bounceInRight', tiempoInicial);
+    tiempoInicial += differenceTime;
+    metaTiempo('.lib', 'bounceInRight', tiempoInicial);
+    tiempoInicial += differenceTime;
+    metaTiempo('.lic', 'bounceInRight', tiempoInicial);
+    tiempoInicial += differenceTime;
+    metaTiempo('.lid', 'bounceInRight', tiempoInicial);
+    tiempoInicial += differenceTime;
+    metaTiempo('.lie', 'bounceInRight', tiempoInicial);
+});
+
+function metaTiempo(clase, animacion, tiempo) {
+    setTimeout(() => {
+        animateCSS(clase, animacion)
+    }, tiempo);
+}
 cambie.addEventListener('click', () => {
     i += 1;
-   
-    if (i % 2 == 0) {  
 
-        animateCSS('.login','bounceOutDown');
-        login.style.opacity="0";
+    if (i % 2 == 0) {
+
+        animateCSS('.login', 'bounceOutDown');
+        login.style.opacity = "0";
         singUp.classList.add('encimade');
-        animateCSS('.signup','bounceInRight');
-        setTimeout(()=>{singUp.style.display = "block";
-        singUp.style.opacity = "1";},600);      
-       
-    } else {  
-        booleano=true;
-        // singUp.style.display = "none";
-        // singUp.style.opacity = "0";
-        animateCSS('.login','bounceInRight');
-        setTimeout(()=>{login.style.opacity="1";singUp.style.display = "none";},600);
-       }
 
-  
+        animateCSS('.signup', 'bounceInRight');
+        setTimeout(() => {
+            singUp.style.display = "block";
+            singUp.style.opacity = "1";
+        }, 600);
+
+    } else {
+        booleano = true;
+        singUp.style.display = "none";
+        singUp.style.opacity = "0";
+
+        animateCSS('.login', 'bounceInRight');
+
+
+        setTimeout(() => {
+            login.style.opacity = "1";
+            singUp.style.display = "0";
+        }, 20);
+    }
+
+
 });
 
 function animateCSS(element, animationName, callback) {
     const node = document.querySelector(element)
-    node.classList.add('animated', animationName,"slow");
+    node.classList.add('animated', animationName, "slow");
 
     function handleAnimationEnd() {
         node.classList.remove('animated', animationName)
